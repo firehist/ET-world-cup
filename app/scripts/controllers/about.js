7acdd37ec9e59eaf7a87c553003829a9bd60a4de
Benjamin LONGEARET <blongearet@teads.tv>
2014-07-03 20:04:22 +0200

    Bootstrap application

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name etWorldCupApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the etWorldCupApp
+ */
+angular.module('etWorldCupApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name etWorldCupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the etWorldCupApp
+ */
+angular.module('etWorldCupApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
