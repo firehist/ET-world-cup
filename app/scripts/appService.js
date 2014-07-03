b325b057094580a94271aaafd819287cd1eeb018
Benjamin LONGEARET <blongearet@teads.tv>
2014-07-03 22:00:31 +0200

    Po

---
+++ app/scripts/appService.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('etWorldCupApp')
+  .factory('appService', function () {
+    'use strict';
+
+    var AppService = function () {
+    };
+
+    AppService.prototype.lol = function () {};
+
+    return new AppService();
+
+  });
--- app/scripts/directives/etchat.js
+++ app/scripts/directives/etchat.js
@@ -8,7 +8,7 @@ angular.module('etWorldCupApp')
       templateUrl: 'views/etchat.html',
       link: function (scope, element, attrs) {
 
-        var ref = new Firebase("https://fiery-fire-9366.firebaseio.com/");
+        var ref = new Firebase("https://fiery-fire-9366.firebaseio.com/etchat");
         scope.messages = $firebase(ref);
         scope.addMessage = function(e) {
           if (e.keyCode != 13) return;
